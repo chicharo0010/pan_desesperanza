e291e1a09f6756ad29daad0f322455bb09889c74
cesar eduardo hernandez hernandez <161785004+chicharo0010@users.noreply.github.com>
2024-12-18 20:01:31 -0600

    Update App.js

--- App.js
+++ App.js
@@ -62,7 +62,9 @@ const con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  multipleStatements: true
+
 });
 
 con.connect((err) => {
