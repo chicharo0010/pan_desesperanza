73136afb1e81427466ca1e468189707b66cbe797
cesar eduardo hernandez hernandez <161785004+chicharo0010@users.noreply.github.com>
2024-12-18 18:55:14 -0600

    Panaderia

--- App.js
+++ App.js
@@ -59,11 +59,10 @@ app.use(session({
 }));
 
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'n0m3l0',
-    database: 'desesperanza',
-    multipleStatements: true
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 con.connect((err) => {
